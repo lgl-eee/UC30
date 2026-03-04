217cd2b476922480c9c27c392024a27317d24a4c
pedro jewur <84276531+lgl-eee@users.noreply.github.com>
2026-03-03 21:59:56 -0300

    Update atividade3.py

--- Atividades/atividade3.py
+++ Atividades/atividade3.py
@@ -1,5 +1,7 @@
 usuario = input(" insira seu usuário: ")
 senha = input("insira sua senha: ")
 
-if(usuario == procopio):
-    
+if(usuario == "procopio" and senha == "12345") or (usuario == "paiva" and senha == "54321"):
+    print ("Seja bem vindo!")
+else:
+    print("Usuário e senha não conferem.") 
