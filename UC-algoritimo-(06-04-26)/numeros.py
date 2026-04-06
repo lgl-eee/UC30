003256b617fa5d60ec594fe43b8ca13ec8b60226
pedro jewur <84276531+lgl-eee@users.noreply.github.com>
2026-04-06 16:07:54 -0300

    Add files via upload

---
+++ UC-algoritimo-(06-04-26)/numeros.py
@@ -0,0 +1,12 @@
+numeros = [a, b, c, d, e, f, g, h]
+contagem = Counter(numeros)
+
+a = int(input("Digite um número: "))
+b = int(input("Digite um número: "))
+c = int(input("Digite um número: "))
+d = int(input("Digite um número: "))
+e = int(input("Digite um número: "))
+f = int(input("Digite um número: "))
+g = int(input("Digite um número: "))
+h = int(input("Digite um número: "))
+
